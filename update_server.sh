73d9d9989eb70ce503a79d3f23bd52b8ca17febe
Dan <oconnordaniel@oconnordaniel.com>
2023-10-16 17:48:38 -0600

    Updated update script

--- update_server.sh
+++ update_server.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 git pull
 JEKYLL_ENV=production bundle exec jekyll b
 
 docker stop dans-homepage
-
+sleep 5
 docker build . -t dans-homepage
+sleep 5
 docker run -d -p 80:80 --name dans-homepage --restart unless-stopped dans-homepage
